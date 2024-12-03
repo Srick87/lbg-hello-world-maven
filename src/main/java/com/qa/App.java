4aff40be06b40f89eecf46fa6150ad1547879b86
srick <sarah.rick87@gmail.com>
2024-12-03 14:03:30 +0000

    EDIT: Team hello

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("SARAH"));
+        System.out.println(sayHelloToSomeone("Team"));
         System.out.println(sayGoodbye());
     }
 
